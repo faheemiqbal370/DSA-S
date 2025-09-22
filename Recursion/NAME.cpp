06bc89c6c79c11e46d55ec45c7d7e27b8ef77e36
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-09-22 20:04:05 +0530

    Recursion

---
+++ Recursion/NAME.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int name(int i,int n){
+    if(i>n) return 0;
+    else{
+    cout<<i<<" faheem"<<endl;
+    name(i+1,n);
+    }
+     //BaseCase
+}
+int main(){
+
+    int n;
+    cout<<"enter how manny times you want to print"<<endl;
+    cin>>n;
+    name(1,n);
+    
+
+}
