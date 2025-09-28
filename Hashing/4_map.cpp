d43b5160d851be12ad5b515165cc32a6ba43af6c
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-09-28 13:12:45 +0530

    MAp

---
+++ Hashing/4_map.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Size"<<endl;
+    cin>>n;
+    map<int,int>mpp;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+        mpp[arr[i]]++;
+    }
+
+    int t;
+    cout<<"how many numbers you wann find"<<endl;
+    cin>>t;
+
+    while(t--){
+        int nbr;
+        cout<<"Enter Number"<<endl;
+        cin>>nbr;
+
+        cout<<"appers "<<mpp[nbr]<<" times"<<endl;
+    }
+}
