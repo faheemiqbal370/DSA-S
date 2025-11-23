f77616d0332f4091148cb626194c49b47b6fb414
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-11-23 12:19:56 +0530

    Bubble Sort

--- Sorting/bubble_sort.cpp
+++ Sorting/bubble_sort.cpp
@@ -4,15 +4,17 @@ using namespace std;
 void bsort(int arr[],int n){
 
     for(int i=n-1;i>=1;i--){
-        
+        int didswap=0;
         for(int j=0;j<=i-1;j++){
             if(arr[j]>arr[j+1]){
 
             int temp = arr[j+1];
             arr[j+1]=arr[j];
             arr[j]=temp;
+            didswap=1;
             }
         }
+        if(didswap==0) break;     // used in best case ..like if array is sorted then no swap happed therefore did swap will be zero.
     }
 }
 int main(){
