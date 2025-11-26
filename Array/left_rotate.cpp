d3182fb37a45031d262517953e749977013a8255
Faheem Iqbal <zargarfaheem4229@gmail.com>
2025-11-26 22:32:43 +0530

    Left Rotate array by 1 place

---
+++ Array/left_rotate.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+int arr[5]={33,543,67,112,98};
+int temp=arr[0];
+int n=5;
+for(int i=1;i<n;i++){
+    arr[i-1]=arr[i];
+}
+arr[n-1]=temp;
+for(int i=0;i<n;i++){
+        cout<<" "<<arr[i];
+    }
+
+}
+
+
+
+
